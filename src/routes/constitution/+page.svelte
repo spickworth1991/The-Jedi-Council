365b41dc17024c129c5c59925558c85d9217bc6e
StickyPicky <45747248+spickworth1991@users.noreply.github.com>
2022-11-10 20:31:14 -0500

    Update +page.svelte

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -294,7 +294,7 @@
     <h3 bind:this={sevenTwo}>7.2 Payout</h3>
     <p>League payout is structured as follows:</p>
     <ul>
-        <li>1st place: {dues * 11}$</li>
+        <li>1st place: {dues * 9}$</li>
         <li>2nd place: {dues}$</li>
     </ul>
     <p>In the event that a manager was fined due to late payment, their extra fee will be added to the winner's payout. Similarly, if new managers come into the league and the payout is smaller, the difference will come out of the winner's prize.</p>
