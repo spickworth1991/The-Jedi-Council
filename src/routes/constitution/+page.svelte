6ac2fd63e278e638274af3542b9974a18c202403
StickyPicky <45747248+spickworth1991@users.noreply.github.com>
2023-01-08 19:57:54 -0500

    Update +page.svelte

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -500,7 +500,7 @@
     <p>Short of continuously failing to set a valid lineup, or confirmed collusion, no manager can be removed against their will. Participation, through the league chat, trade offers, and the waiver wire, is strongly encouraged but generally NOT grounds for removal.</p>
     
     <h3 bind:this={sixTwo}>7.2 Replacing Managers</h3>
-    <p>When a manager needs to be replaced, the commisiooners will try to find a suitable candidate with some connection to at least a portion of the existing managers. Priority will be placed on managers who are eager to take on a dynasty commitment and will be very active within the league.</p>
+    <p>When a manager needs to be replaced, the commissioners will try to find a suitable candidate with some connection to at least a portion of the existing managers. Priority will be placed on managers who are eager to take on a dynasty commitment and will be very active within the league.</p>
     
     <h3 bind:this={sixThree}>7.3 Replacement Incentive</h3>
     <p>In the event that a manager chooses to quit the league or is removed by a comissioner, an incoming manager is offered a 50% reduction on their first year buy-in. If the year isnt already paid for.</p>
